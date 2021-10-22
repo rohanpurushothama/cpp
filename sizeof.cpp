c4098df6df2ad97bde8b4940832d2c2dd2b2c09d
rohanpurushothama <rohanpurushothama@gmail.com>
2021-10-22 14:56:22 +0530

    First commit

---
+++ sizeof.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    cout << "size of char:" << sizeof(char) << " byte" << endl;
+
+    cout << "size of int: " << sizeof(int) << " byte" << endl;
+
+    cout << "size of int: " << sizeof(int) << " byte" << endl;
+}
---
+++ variables.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+    //including integer variables
+    int a;
+    a = 10;
+
+    cout << a << endl;
+}
